ac4200e430bb6c4764ffb6185ef41683034f3089
RaviTejaVattem <ravitejavattem@gmail.com>
2022-02-20 21:05:40 +0530

    Added devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HelloComponent } from './hello.component';
 import { StoreModule } from '@ngrx/store';
 import { addProductReducer } from './reducer';
 import { ProductComponent } from './product/product.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     StoreModule.forRoot({ product: addProductReducer }),
+    StoreDevtoolsModule.instrument(),
   ],
   declarations: [AppComponent, HelloComponent, ProductComponent],
   bootstrap: [AppComponent],
