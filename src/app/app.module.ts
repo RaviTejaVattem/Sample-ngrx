4ed4d994b3385835bb71b23f759c342129c55379
RaviTejaVattem <ravitejavattem@gmail.com>
2022-02-20 20:56:08 +0530

    Basic store changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { StoreModule } from '@ngrx/store';
+import { addProductReducer } from './reducer';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ product: addProductReducer }),
+  ],
+  declarations: [AppComponent, HelloComponent, ProductComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
